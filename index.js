0777e12b281c52b34ed9764ed3789451e9152b4d
Wesley Schleumer <wesley.schleumer@gmail.com>
2013-09-18 20:16:49 +0000

    :heart:

--- index.js
+++ index.js
@@ -9,83 +9,87 @@ var _ = require('underscore');
 var str = require('underscore.string');
 
 var octopus = {
-	path: path.normalize('/var/www/rrlengenharia/'),
+	path: path.normalize('/var/www/node-guiadolugar/'),
 	remotePath: 'test/',
 	branch: 'master',
 	dest: 'origin',
 	files: []
 }
 
-function puts(error, stdout, stderr) { console.log(stdout) }
-
-process.chdir(octopus.path);
-exec("git reset --hard");
-exec("git pull "+ octopus.dest + " " + octopus.branch, puts);
-
-function mkfile(c, source, file, callback){
-	var path = null;
-	if(file.toString().indexOf('/') !== -1){
-		sfile = file.split('/');
-		sfile.pop();
-		path = sfile.join('/');
-	}
-	function put(file){
-		c.put(source, file, callback)
-	}
-	if(path){
-		c.mkdir(path, true, function(err){
-			if(!err){
+octopus.init = function(){
+	function mkfile(c, source, file, callback){
+		var path = null;
+		if(file.toString().indexOf('/') !== -1){
+			sfile = file.split('/');
+			sfile.pop();
+			path = sfile.join('/');
+		}
+		function put(file){
+			c.put(source, file, callback)
+		}
+		if(path){
+			c.mkdir(path, true, function(err){
 				put(file);
-			}else{
-				callback(err);
-			}
-		});
-	}else{
-		put(file);
-	}
-}
-
-
-var c = new Client();
-c.on('ready', function() {
-	c.get('.last-deploy', function(err, stream){
-		if(!err){
-			var version = '';
-			stream.on('data', function (chunk) {
-				version += chunk;
-			});
-			stream.on('end', function(){
-				console.log(version);
 			});
 		}else{
-			var walker  = walk.walk(octopus.path, { followLinks: false });
-			walker.on('file', function(root, stat, next) {
-				var fileName = stat.name;
-				var trueRoot = (root.replace(octopus.path,'') + path.sep + fileName).replace(/^\/|^\\/g,'');
-				var pathname = path.join(root, fileName);
-				if(!/^\/?\.git/.test(trueRoot)){
-					mkfile(c, pathname, trueRoot, function(err){
+			put(file);
+		}
+	}
+
+
+	var c = new Client();
+	c.on('ready', function() {
+		c.get('.last-deploy', function(err, stream){
+			if(!err){
+				var version = '';
+				stream.on('data', function (chunk) {
+					version += chunk;
+				});
+				stream.on('end', function(){
+					console.log(version);
+				});
+			}else{
+				var walker  = walk.walk(octopus.path, { followLinks: false });
+				walker.on('file', function(root, stat, next) {
+					var fileName = stat.name;
+					var trueRoot = (root.replace(octopus.path,'') + path.sep + fileName).replace(/^\/|^\\/g,'');
+					var pathname = path.join(root, fileName);
+					if(!/^\/?\.git\//.test(trueRoot)){
+						mkfile(c, pathname, trueRoot, function(err){
 						if(!err){
 							//console.log(trueRoot + ' enviado');
-							process.stdout.write(str.pad("\renviado: " + trueRoot, 100, ' '));
+							process.stdout.write(str.pad("\nenviado: " + trueRoot, 100, ' '));
 						}else{
 							console.log("\n", stat, trueRoot, err, ' não enviado\n');
 						}
 						next();
 					});
-				}else{
-					next();
-				}
-			});
-		}
+					}else{
+						next();
+					}
+				});
+				walker.on('end', function(){
+					c.end();
+				});
+			}
+		});
+	});
+	c.connect({
+		host: test.host,
+		port: 21,
+		user: test.user,
+		password: test.password,
+		keepalive: true
+	});
+}
+
+
+
+
+process.chdir(octopus.path);
+exec("git reset --hard", function(){
+	exec("git pull "+ octopus.dest + " " + octopus.branch, function(){
+		octopus.init();
 	});
 });
 
-
-c.connect({
-	host: test.host,
-	port: 21,
-	user: test.user,
-	password: test.password,
-	keepalive: true
-});
